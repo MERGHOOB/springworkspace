43af10c8b36fbcc24efa8390812fff9c929d2c3d
mekhan <margub.khan@gmail.com>
2020-06-12 00:46:10 +0530

    model to table, creation of entities

---
+++ flightreservation/src/main/java/com/margub/flightreservation/entities/Reservation.java
@@ -0,0 +1,45 @@
+package com.margub.flightreservation.entities;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Reservation extends AbstractEntity {
+
+	private Boolean checkedIn;
+	private int numberOfBags;
+	private Passanger passanger;
+	private Flight flight;
+
+	public Boolean getCheckedIn() {
+		return checkedIn;
+	}
+
+	public void setCheckedIn(Boolean checkedIn) {
+		this.checkedIn = checkedIn;
+	}
+
+	public int getNumberOfBags() {
+		return numberOfBags;
+	}
+
+	public void setNumberOfBags(int numberOfBags) {
+		this.numberOfBags = numberOfBags;
+	}
+
+	public Passanger getPassanger() {
+		return passanger;
+	}
+
+	public void setPassanger(Passanger passanger) {
+		this.passanger = passanger;
+	}
+
+	public Flight getFlight() {
+		return flight;
+	}
+
+	public void setFlight(Flight flight) {
+		this.flight = flight;
+	}
+
+}
