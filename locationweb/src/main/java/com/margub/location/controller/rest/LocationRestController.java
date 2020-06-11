bf3962312daba9f94d85a67277f43e1e9f7845a9
mekhan <margub.khan@gmail.com>
2020-06-11 22:53:01 +0530

    SEC-8 Integeration Layer
    Setting the RestController

---
+++ locationweb/src/main/java/com/margub/location/controller/rest/LocationRestController.java
@@ -0,0 +1,10 @@
+package com.margub.location.controller.rest;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/locations")
+public class LocationRestController {
+
+}
