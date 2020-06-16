2302a9b5febbeedff6384ad826e2e7b1e36a6254
mekhan <margub.khan@gmail.com>
2020-06-16 08:51:33 +0530

    provide functionality- update client

--- flightcheckin/src/main/java/com/margub/flightcheckin/integration/ReservationRestClientImpl.java
+++ flightcheckin/src/main/java/com/margub/flightcheckin/integration/ReservationRestClientImpl.java
@@ -7,18 +7,24 @@ import com.margub.flightcheckin.integration.dto.ReservationUpdateRequest;
 
 public class ReservationRestClientImpl implements IReservationRestClient {
 
+	private static final String FLIGHT_RESERVATION_APP_URL = "http://localhost:8080/flightreservation/reservations/";
+
 	@Override
 	public Reservation findReservation(Long id) {
 		RestTemplate restTemplate = new RestTemplate();
 		Reservation reservation = restTemplate
-				.getForObject("http://localhost:8080/flightreservation/reservations/" + id, Reservation.class);
+				.getForObject(FLIGHT_RESERVATION_APP_URL + id, Reservation.class);
 
 		return reservation;
 	}
 
 	@Override
 	public Reservation updateReservation(ReservationUpdateRequest request) {
-		return null;
+		RestTemplate restTemplate = new RestTemplate();
+		Reservation updatedReservation = restTemplate
+				.postForObject(FLIGHT_RESERVATION_APP_URL, request, Reservation.class);
+
+		return updatedReservation;
 	}
 
 }
