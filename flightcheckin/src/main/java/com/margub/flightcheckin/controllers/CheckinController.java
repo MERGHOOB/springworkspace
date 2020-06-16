8820f589807b673d11623f1c087096dbb7aabd76
mekhan <margub.khan@gmail.com>
2020-06-16 09:03:06 +0530

    Handle start Checkin

--- flightcheckin/src/main/java/com/margub/flightcheckin/controllers/CheckinController.java
+++ flightcheckin/src/main/java/com/margub/flightcheckin/controllers/CheckinController.java
@@ -1,14 +1,31 @@
 package com.margub.flightcheckin.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.margub.flightcheckin.integration.IReservationRestClient;
+import com.margub.flightcheckin.integration.dto.Reservation;
 
 @Controller
 public class CheckinController {
 
+	@Autowired
+	IReservationRestClient reservationRestClient;
+
 	@RequestMapping("/showStartCheckin")
 	public String showStartCheckin() {
 		return "startCheckin";
+	}
+
+	@RequestMapping("/startCheckin")
+	public String startCheckin(@RequestParam("reservationId") Long reservationId, ModelMap modelMap) {
+
+		Reservation reservation = reservationRestClient.findReservation(reservationId);
+		modelMap.addAttribute("reservation", reservation);
+		return "displayReservationDetails";
 	}
 
 }
--- flightcheckin/src/main/java/com/margub/flightcheckin/integration/ReservationRestClientImpl.java
+++ flightcheckin/src/main/java/com/margub/flightcheckin/integration/ReservationRestClientImpl.java
@@ -1,10 +1,12 @@
 package com.margub.flightcheckin.integration;
 
+import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
 import com.margub.flightcheckin.integration.dto.Reservation;
 import com.margub.flightcheckin.integration.dto.ReservationUpdateRequest;
 
+@Component
 public class ReservationRestClientImpl implements IReservationRestClient {
 
 	private static final String FLIGHT_RESERVATION_APP_URL = "http://localhost:8080/flightreservation/reservations/";
