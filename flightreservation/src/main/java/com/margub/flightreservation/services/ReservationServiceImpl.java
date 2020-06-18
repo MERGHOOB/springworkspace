f213fa7edc686a448b58e8d3e254f54cc7cccad9
mekhan <margub.khan@gmail.com>
2020-06-18 19:56:36 +0530

    reservation service to use PDFGenerator and send email

--- flightreservation/src/main/java/com/margub/flightreservation/services/ReservationServiceImpl.java
+++ flightreservation/src/main/java/com/margub/flightreservation/services/ReservationServiceImpl.java
@@ -10,39 +10,61 @@ import com.margub.flightreservation.entities.Reservation;
 import com.margub.flightreservation.repos.IFlightRepository;
 import com.margub.flightreservation.repos.IPassengerRepository;
 import com.margub.flightreservation.repos.IReservationRepository;
+import com.margub.flightreservation.utils.mail.EmailUtil;
+import com.margub.flightreservation.utils.pdf.PDFGenerator;
 
 @Service
 public class ReservationServiceImpl implements IReservationService {
 
+	private static final String UNDERSCORE = "_";
+
+	private static final String RESERVATION_FOLDER_PATH = "C:\\margub\\training\\Documents\\reservations\\";
+
 	@Autowired
 	IFlightRepository flightRepository;
-	
+
 	@Autowired
 	IPassengerRepository passengerRepository;
-	
+
 	@Autowired
 	IReservationRepository reservationRepository;
-	
+
+	@Autowired
+	PDFGenerator pdfGenerator;
+
+	@Autowired
+	EmailUtil emailUtil;
+
 	@Override
 	public Reservation bookFlight(ReservationRequest reservationRequest) {
 		long flightId = reservationRequest.getFlightId();
 		Flight flight = flightRepository.getOne(flightId);
-		
+
 		Passenger passenger = new Passenger();
 		passenger.setFirstName(reservationRequest.getPassengerFirstName());
 		passenger.setLastName(reservationRequest.getPassengerLastName());
 		passenger.setEmail(reservationRequest.getPassengerEmail());
 		passenger.setPhone(reservationRequest.getPassengerPhone());
-		
+
 		Passenger savedPassenger = passengerRepository.save(passenger);
-		
+
 		Reservation reservation = new Reservation();
 		reservation.setFlight(flight);
 		reservation.setPassenger(savedPassenger);
-		
+
 		Reservation savedReservation = reservationRepository.save(reservation);
-		
+
+		String pdFpath = getPDFpath(savedReservation);
+		pdfGenerator.generateItinerary(savedReservation, pdFpath);
+
+		emailUtil.sendItinerary(savedReservation.getPassenger().getEmail(), pdFpath);
+
 		return savedReservation;
+	}
+
+	private String getPDFpath(Reservation savedReservation) {
+		return RESERVATION_FOLDER_PATH + savedReservation.getFlight().getDepartureCity() + UNDERSCORE
+				+ savedReservation.getFlight().getArrivalCity() + UNDERSCORE + savedReservation.getId() + ".pdf";
 	}
 
 }
