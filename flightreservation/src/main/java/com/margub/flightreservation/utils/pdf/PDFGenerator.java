0464e8214fef4bb3d830693213a36a3264cef355
mekhan <margub.khan@gmail.com>
2020-06-18 18:57:09 +0530

    Add flight details to PDF

--- flightreservation/src/main/java/com/margub/flightreservation/utils/pdf/PDFGenerator.java
+++ flightreservation/src/main/java/com/margub/flightreservation/utils/pdf/PDFGenerator.java
@@ -7,6 +7,8 @@ import org.springframework.stereotype.Component;
 
 import com.itextpdf.text.Document;
 import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Phrase;
+import com.itextpdf.text.pdf.PdfPCell;
 import com.itextpdf.text.pdf.PdfPTable;
 import com.itextpdf.text.pdf.PdfWriter;
 import com.margub.flightreservation.entities.Reservation;
@@ -20,16 +22,53 @@ public class PDFGenerator {
 
 		try {
 			PdfWriter.getInstance(document, new FileOutputStream(filePath));
-			
+
 			document.open();
 
-			document.add(new PdfPTable(2));
+			document.add(generateTable(reservation));
 
 			document.close();
 
 		} catch (FileNotFoundException | DocumentException e) {
 			e.printStackTrace();
 		}
+	}
+
+	private PdfPTable generateTable(Reservation reservation) {
+
+		int maxColumnInTable = 2;
+		PdfPTable table = new PdfPTable(maxColumnInTable);
+
+		PdfPCell flightItenaryCell = new PdfPCell(new Phrase("Flight Itinerary"));
+		flightItenaryCell.setColspan(maxColumnInTable);
+
+		table.addCell(flightItenaryCell);
+
+		// ---------------Flight details
+
+		PdfPCell flightDetailsCell = new PdfPCell(new Phrase("Flight Details"));
+		flightDetailsCell.setColspan(maxColumnInTable);
+
+		table.addCell(flightDetailsCell);
+		
+		// Adding flight details taking updating value from reservation
+		
+		table.addCell("Departure City");
+		table.addCell(reservation.getFlight().getDepartureCity());
+		
+		table.addCell("Arrival City");
+		table.addCell(reservation.getFlight().getArrivalCity());
+		
+		table.addCell("Flight Number");
+		table.addCell(reservation.getFlight().getFlightNumber());
+		
+		table.addCell("Departure Date");
+		table.addCell(reservation.getFlight().getDateOfDeparture().toString());	
+		
+		table.addCell("Departure time");
+		table.addCell(reservation.getFlight().getEstimatedDepartureTime().toString());	
+
+		return table;
 	}
 
 }
