b1d8530c041c6027234830028fb87a9ef0c0c356
mekhan <margub.khan@gmail.com>
2020-06-18 18:48:13 +0530

    adding pdf generator and its dependency

---
+++ flightreservation/src/main/java/com/margub/flightreservation/utils/pdf/PDFGenerator.java
@@ -0,0 +1,35 @@
+package com.margub.flightreservation.utils.pdf;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+
+import org.springframework.stereotype.Component;
+
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.pdf.PdfPTable;
+import com.itextpdf.text.pdf.PdfWriter;
+import com.margub.flightreservation.entities.Reservation;
+
+@Component
+public class PDFGenerator {
+
+	public void generateItinerary(Reservation reservation, String filePath) {
+
+		Document document = new Document();
+
+		try {
+			PdfWriter.getInstance(document, new FileOutputStream(filePath));
+			
+			document.open();
+
+			document.add(new PdfPTable(2));
+
+			document.close();
+
+		} catch (FileNotFoundException | DocumentException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
